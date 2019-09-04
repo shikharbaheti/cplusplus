055db522703b4239467badf5e5a20d13970afb46
Shikhar Baheti <46034575+shikharbaheti@users.noreply.github.com>
2019-09-04 13:07:05 -0500

    uploaded lab 2

---
+++ Lab/Lab2/problem1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num;
+  int lower = 0;
+
+  cin >> num;
+
+if (num == 0){
+  cout << "empty";
+}
+while (num != 0 ){
+    if (num < lower) {
+      num = lower;
+      cin >> num;
+    }
+    else if (num > lower){
+
+    }
+  }
+
+cout << "The lowest number is: " << lower << endl;
+
+return 0;
+
+}
