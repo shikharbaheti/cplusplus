6eb91828b64e7df44aa78bfc0d93f69605b77d0a
Shikhar Baheti <46034575+shikharbaheti@users.noreply.github.com>
2019-11-21 13:36:05 -0600

    doubly linked lists

---
+++ Homework/Homework11/DoublyLinkedList.h
@@ -0,0 +1,98 @@
+#ifndef DOUBLYLINKEDLIST_H
+#define DOUBLYLINKEDLIST_H
+#include "Node.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+//////////////////////////////////////////////////////////////////////////////
+//                         class template definition                        //
+//////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+class DoublyLinkedList {
+public:
+    /* uncomment the declarations as you implement them */
+    DoublyLinkedList();
+    // DoublyLinkedList(T);
+    // DoublyLinkedList(const DoublyLinkedList<T>&);
+    // DoublyLinkedList<T>& operator=(const DoublyLinkedList<T>&);
+    // ~DoublyLinkedList();
+    // unsigned int size() const { return sz; }
+    // T& front();
+    // const T& front() const;
+    // T& back();
+    // const T& back() const;
+    // void push_back(T);
+    // void push_front(T);
+    // void pop_back();
+    // void pop_front();
+    // void erase(unsigned int);
+    // void insert(T data, unsigned int idx);
+    // void clear();
+    std::string to_str() const;
+    // breaks encapsulation... gives operator== access to private data... not great approach, but makes the homework easier for you since we haven't talked about iterators :).
+    // template<typename U> friend bool operator==(DoublyLinkedList<U> const&, DoublyLinkedList<U> const&);
+private:
+    // do not change ordering.
+    Node<T> *head;
+    Node<T> *tail;
+    unsigned int sz;
+};
+
+//Doing as the prompt says
+template <typename T> DoublyLinkedList<T>::DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+//////////////////////////////////////////////////////////////////////////////
+//                       helper function declarations                       //
+//////////////////////////////////////////////////////////////////////////////
+template<typename T> std::ostream& operator<<(std::ostream &, DoublyLinkedList<T> const&);
+
+template<typename T> bool operator==(DoublyLinkedList<T> const&, DoublyLinkedList<T> const&);
+
+template<typename T> bool operator!=(DoublyLinkedList<T> const&, DoublyLinkedList<T> const&);
+
+//////////////////////////////////////////////////////////////////////////////
+//                     member template function definitions                 //
+//////////////////////////////////////////////////////////////////////////////
+template<typename T>
+std::string DoublyLinkedList<T>::to_str() const
+{
+    std::stringstream os;
+    const Node<T> *curr = head;
+    os << std::endl << std::setfill('-') << std::setw(80) << '-' << std::setfill(' ') << std::endl;
+    os << "head: " << head << std::endl;
+    os << "tail: " << tail << std::endl;
+    os << "  sz: " << sz << std::endl;
+    os << std::setw(16) << "node" << std::setw(16) << "node.prev" << std::setw(16) << "node.data" <<  std::setw(16) << "node.next" << std::endl;
+    while (curr) {
+        os << std::setw(16) << curr;
+        os << std::setw(16) << curr->prev;
+        os << std::setw(16) << curr->data;
+        os << std::setw(16) << curr->next;
+        os << std::endl;
+        curr = curr->next;
+    }
+    os << std::setfill('-') << std::setw(80) << '-' << std::setfill(' ') << std::endl;
+    return os.str();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//                     helper template function definitions                 //
+//////////////////////////////////////////////////////////////////////////////
+template<typename T>
+bool operator==(DoublyLinkedList<T> const& lhs, DoublyLinkedList<T> const& rhs)
+{
+    // getting you started on this once... definition for this overloaded operator will go here.
+    return true;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, DoublyLinkedList<T> const& rhs)
+{
+    os << rhs.to_str();
+    return os;
+}
+
+#endif
---
+++ Homework/Homework11/Node.h
@@ -0,0 +1,33 @@
+#ifndef NODE_H
+#define NODE_H
+
+//////////////////////////////////////////////////////////////////////////////
+//                         class template definition                        //
+//////////////////////////////////////////////////////////////////////////////
+template<typename T>
+struct Node {
+    Node(T data); // need to define this
+    T data;
+    Node* next;
+    Node* prev;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+//                       helper function declarations                       //
+//////////////////////////////////////////////////////////////////////////////
+template<typename T> bool operator==(const Node<T>& rhs, const Node<T>& lhs);
+template<typename T> bool operator!=(const Node<T>& rhs, const Node<T>& lhs);
+template<typename T> bool operator<(const Node<T>& rhs, const Node<T>& lhs);
+template<typename T> bool operator<=(const Node<T>& rhs, const Node<T>& lhs);
+template<typename T> bool operator>(const Node<T>& rhs, const Node<T>& lhs);
+template<typename T> bool operator>=(const Node<T>& rhs, const Node<T>& lhs);
+
+//////////////////////////////////////////////////////////////////////////////
+//                     member template function definitions                 //
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+//                     helper template function definitions                 //
+//////////////////////////////////////////////////////////////////////////////
+
+#endif
